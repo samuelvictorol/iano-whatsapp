edd44a92864bc04aedd2de62b4e1b110ab3cda48
samuelvictorol <samuel.softdev@outlook.com>
2025-11-28 16:39:28 -0300

    [HOTFIX] Update baseUrl render api url

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,8 +1,7 @@
 import { defineBoot } from '#q-app/wrappers'
 import axios from 'axios'
-const baseURL = 'http://localhost:10000'
-const api = axios.create({ baseURL: baseURL })
-console.log('🟢 API -> ' + baseURL)
+
+const api = axios.create({ baseURL: 'https://iano-whatsapp-backend.onrender.com' })
 export default defineBoot(({ app }) => {
   app.config.globalProperties.$axios = axios
   app.config.globalProperties.$api = api
